fcc0d587436cbe15d967678915945be67e1b1893
Kauê Boscariol <120749036+kauebbs@users.noreply.github.com>
2025-04-02 12:46:02 -0300

    Update CorpoHumano.java

--- src/main/java/CorpoHumano.java
+++ src/main/java/CorpoHumano.java
@@ -1,18 +1,30 @@
 package main.java;
+
+class CorpoHumano
+{
+    public float massa;
+    private  float altura;
+    public float IMC;
+
+    public void SetMassa(float massa)
+    {
+        this.massa = massa;
+    }
+
+    public void SetAltura(float altura)
+    {
+        this.altura = altura;
+    }
+
+    public void setIMC(float IMC){
+        this.IMC = IMC;
+    }
  
-import java.util.Scanner;
- 
-public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner read = new Scanner(System.in);
-   
-        CorpoHumano c1 = new CorpoHumano();
-        System.out.println("Digite o seu peso: ");
-        c1.SetMassa(read.nextFloat());
-        System.out.println("Digite sua altura: ");
-        c1.SetAltura(read.nextFloat());
-        c1.calcularIMC();
-        
-        System.out.println(c1.getIMC());
+    public void calcularIMC(){
+        IMC = massa/(altura*altura);
+    }
+
+    public float getIMC(){
+        return this.IMC;
     }
 }
