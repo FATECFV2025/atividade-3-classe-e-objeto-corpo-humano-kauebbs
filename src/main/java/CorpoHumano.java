61c991d8a3a838adf89e96033626d8c801e71f47
Kauê Boscariol <120749036+kauebbs@users.noreply.github.com>
2025-04-02 12:45:35 -0300

    Update CorpoHumano.java

--- src/main/java/CorpoHumano.java
+++ src/main/java/CorpoHumano.java
@@ -1,5 +1,18 @@
 package main.java;
-
-public class CorpoHumano {
-
+ 
+import java.util.Scanner;
+ 
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner read = new Scanner(System.in);
+   
+        CorpoHumano c1 = new CorpoHumano();
+        System.out.println("Digite o seu peso: ");
+        c1.SetMassa(read.nextFloat());
+        System.out.println("Digite sua altura: ");
+        c1.SetAltura(read.nextFloat());
+        c1.calcularIMC();
+        
+        System.out.println(c1.getIMC());
+    }
 }
