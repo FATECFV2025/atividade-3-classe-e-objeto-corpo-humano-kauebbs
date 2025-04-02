24bdcb2466a423a815c31f44cb147cd6dd4cb4bd
Kauê Boscariol <120749036+kauebbs@users.noreply.github.com>
2025-04-02 12:46:31 -0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,30 +1,18 @@
 package main.java;
+ 
 import java.util.Scanner;
-
+ 
 public class App {
-        //Comentário
-
-    public static void funcao_teste (){
-        //Biblioteca para entrada de dados
-        Scanner ler = new Scanner(System.in); 
-
-        //variável local a
-        String v2;
-        int v1;
-
-        System.out.println("Comando de Saida de Dados");
-        
-        System.out.printf("Informe uma palavra: ");
-        v2 = ler.next();
-
-        System.out.printf("Informe um número: ");
-        v1 = ler.nextInt();
-
-        System.out.printf(v2+" "+v1);
-    }
     public static void main(String[] args) throws Exception {
-        funcao_teste();
-
-
+        Scanner read = new Scanner(System.in);
+   
+        CorpoHumano c1 = new CorpoHumano();
+        System.out.println("Digite o seu peso: ");
+        c1.SetMassa(read.nextFloat());
+        System.out.println("Digite sua altura: ");
+        c1.SetAltura(read.nextFloat());
+        c1.calcularIMC();
+        
+        System.out.println(c1.getIMC());
     }
 }
